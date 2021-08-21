5ec05c5a8b09102146aab6be07c633d94bfd3f54
wonkyum-kim <67191801+wonkyum-kim@users.noreply.github.com>
2021-08-21 17:47:54 +0900

    Create exercise 3_1.c

---
+++ chapter 3/exercise 3_1.c
@@ -0,0 +1,18 @@
+#include <stdio.h>
+#include <unistd.h>
+#include <wait.h>
+
+int value = 5;
+
+int main()
+{
+    pid_t pid;
+    pid = fork();
+    if (pid == 0) {  // child process
+        value += 15;
+    }
+    else if (pid > 0) { // parnet process
+        wait(NULL);
+        printf("Parent: value = %d\n", value);  // line A
+    }
+}
